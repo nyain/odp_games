a92464762dedb6f9e3c3c6e5703e673b8dc975b8
Vincent <minimal6huruf@gmail.com>
2025-01-14 16:54:18 +0700

    Armstrong Number: DONE

--- games/b.py
+++ games/b.py
@@ -1,14 +1,10 @@
-
 def b():
     """
     Diberikan sebuah list angka, cari pasangan angka dengan selisih terkecil.
 
-    MASUKAN
+    contoh MASUKAN
     data = [4, 8, 15, 16, 23, 42] 
 
     KELUARAN
     15 16
     """
-
-    # write the code solution here
-    print("Mohon maaf, permainan B belum tersedia!")
